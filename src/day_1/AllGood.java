5cc3d98fc49009ba3a61825a83960f1294ef18f1
aigulechka <64097912+aigulechka@users.noreply.github.com>
2020-04-24 14:29:52 -0600

    Update AllGood.java

--- src/day_1/AllGood.java
+++ src/day_1/AllGood.java
@@ -5,6 +5,9 @@ public class AllGood {
     public static void main(String[] args) {
 
         //I hope everything is GOOD.
+        
+        //this is addedffrom GitHub
+        
     }
 }
-// tets
+// 
