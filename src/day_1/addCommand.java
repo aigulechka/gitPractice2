12f249c33026e3174fba0840e44c1f4838e6f495
aigulechka <64097912+aigulechka@users.noreply.github.com>
2020-04-28 13:49:21 -0600

    Update addCommand.java

--- src/day_1/addCommand.java
+++ src/day_1/addCommand.java
@@ -4,5 +4,8 @@ public class addCommand {
     public static void main(String[] args) {
         //will see you in Github
         //I made changes when I created addCommand class
+        
+        
+        ///hhhhhhhhhhhhh
     }
 }
