2d642eee0baa08d296da1f033b889e3647e8e44b
aigulechka <aigulechka2006@yahoo.com>
2020-04-28 13:26:27 -0600

    add addCommand.java

---
+++ src/day_1/addCommand.java
@@ -0,0 +1,8 @@
+package day_1;
+
+public class addCommand {
+    public static void main(String[] args) {
+        //will see you in Github
+        //I made changes when I created addCommand class
+    }
+}
